c389c5d223243d59a38dc4cf54e9f766ec426099
unknown <lillianngoiri67@gmail.com>
2024-03-01 16:07:47 +0300

    Have added the dart function assignment

---
+++ functions.dart
@@ -0,0 +1,77 @@
+import 'dart:ffi';
+
+
+
+  //Task 1
+  //Write a function called addTwo that takes two numbers as arguments and returns the sum of those two numbers.
+
+/*     int addTwo(int a, int b) {
+      return a + b;
+    }
+
+    void main() {
+      int result = addTwo(10, 20);
+      print("The sum is: $result");
+    }
+ */
+  //Task 2
+  //Write a function called subtractTwo that takes two numbers as arguments and returns the difference of those two numbers.
+
+   /*  int subtractTwo(int x, int y) {
+      return x - y;
+    }
+    void main(){
+      int difference = subtractTwo(70, 30);
+      print("The difference is: $difference");
+    } */
+
+  //Task 3
+  //Write a function called multiplyTwo that takes two numbers as arguments and returns the product of those two numbers.
+
+   /*  int multiplyTwo(int a, int b) {
+      return a * b;
+    }
+
+    void main() {
+      int product = multiplyTwo(10, 20);
+      print("The product is: $product");
+    } */
+
+
+
+  //Task 4
+  //Write a function called divideTwo that takes two numbers as arguments and returns the quotient of those two numbers.
+
+  // parameter and return type
+/*     double divideTwo(double x, double y) {
+      return x / y;
+    }
+    void main(){
+      double quotient = divideTwo(100, 20);
+        print("The quotient is: $quotient");
+    } */
+
+  //Task 5
+  //Write a function called stringLength that takes an argument of type String and returns the length of that string.
+
+  int stringLength(String names) {
+    return names.length;
+  }
+
+  //void main() {
+    // Example usage
+    //String names = "Hello, Lilian Mwangi!";
+    //int length = stringLength(names);
+    //print("Length of the string is: $length");
+  //}
+
+  //Task 6
+  //Write a function called getFirstElement that takes a list as an argument and returns the first element of that list.
+      void main() {
+    List<String> getFirstElement = ["JAVA", "PHP", "SQL"];
+    //print("Value of names is $getFirstElement");
+    print("The first element is ${getFirstElement[0]}"); 
+
+    print(getFirstElement);
+    }
+
